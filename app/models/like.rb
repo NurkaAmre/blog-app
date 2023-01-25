3d7138a8b6b5970df4f1bb40d63b1476e1b83f5b
NurkaAmre <nurkerey30@gmail.com>
2023-01-25 23:35:42 +0600

    Method that updates the likes counter for a post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  belongs_to :comment
+  after_save :increment_likes_counter
+  
+  private
+  def increment_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
