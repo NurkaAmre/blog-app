9456645d5a0a5505a450788bd8baaec24af283af
NurkaAmre <nurkerey30@gmail.com>
2023-01-25 23:28:31 +0600

    Method updates postCounter for user &  method return 5most recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  alter_save :increment_posts_counter
+
+  def increment_posts_counter
+    user.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
