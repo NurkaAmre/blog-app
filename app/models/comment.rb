810221bc7faffc19fe8a7731e7883a04e457d5f0
NurkaAmre <nurkerey30@gmail.com>
2023-01-26 17:29:01 +0600

    Fix rubocop errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   after_save :increment_post_comments_counter
-  
+
   private
+
   def increment_post_comments_counter
     post.increment!(:comments_counter)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,9 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
   after_save :increment_likes_counter
-  
+
   private
+
   def increment_likes_counter
     post.increment!(:likes_counter)
   end
