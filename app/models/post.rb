a9043073e63f65a2cacd2f05c0263735c67bdcba
NurkaAmre <nurkerey30@gmail.com>
2023-01-27 13:54:29 +0600

    Add validation for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   after_save :increment_posts_counter
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def increment_posts_counter
     user.increment!(:posts_counter)
   end
