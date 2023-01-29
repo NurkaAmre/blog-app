14adc4557f590da294af2cb77f62e760d74e56c8
NurkaAmre <nurkerey30@gmail.com>
2023-01-30 01:03:20 +0600

    Fix rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,9 @@
 class PostsController < ApplicationController
-    def index;
-        @posts = Post.all.order(created_at DESC)
-    end
+  def index
+    @posts = Post.all.order(created_at(DESC))
+  end
 
-    def show;
-        @post = Post.find(params[:id])
-    end
-    
+  def show
+    @post = Post.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
   
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
