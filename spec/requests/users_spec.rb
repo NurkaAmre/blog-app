98f7e42ed6e970a5ab0ac49a0f8094fcdabf6818
NurkaAmre <nurkerey30@gmail.com>
2023-01-31 21:40:20 +0600

    The best verion of testing

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,19 +5,37 @@ Rails::Controller::Testing.install
 RSpec.describe UsersController, type: :request do
   describe 'GET #index' do
     it 'returns a successful response' do
-      get '/users'
+      get users_path
       expect(response).to be_successful
       expect(response).to have_http_status(200)
     end
 
     it 'renders the correct template' do
-      get '/users'
+      get users_path
       expect(response).to render_template(:index)
     end
 
     it 'includes the correct placeholder text' do
-      get '/users'
+      get users_path
       expect(response.body).to include('List of users')
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns a successful response' do
+      get '/users/1'
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      get '/users/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text' do
+      get '/users/1'
+      expect(response.body).to include('I am user show')
     end
   end
 end
