32a4e50861c478c94f0fdf05ddc03e7bc0c3ce14
NurkaAmre <nurkerey30@gmail.com>
2023-02-07 01:25:03 +0600

    RSpec for users index

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'User index page', type: :feature do
   describe 'User index page process' do
     before(:each) do
-      @user = User.create(name: 'mila', photo: 'https://i.imgur.com/1JZ1Q2r.jpg', bio: 'I am a biology teacher',
-                          posts_counter: 2)
+      @user = User.create(name: 'Amre', photo: 'https://google.com/amirr.jpg', bio: 'I am a scientist Amre',
+                          PostCounter: 2)
       visit users_path
     end
 
@@ -17,11 +17,11 @@ RSpec.describe 'User index page', type: :feature do
     end
 
     it 'should show the number of posts of all other users' do
-      expect(page.body).to include(@user.posts_counter.to_s)
+      expect(page.body).to include(@user.PostCounter.to_s)
     end
 
-    it "When I click on a user, should redirected to that user's show page." do
-      click_link(@user.name)
+    it "When I click See details, should redirected to that user's show page." do
+      click_link("See details")
       expect(page).to have_current_path(user_path(@user.id))
     end
   end
