5bae857e357fa343a9fd097356b20070f5042012
NurkaAmre <nurkerey30@gmail.com>
2023-02-06 15:15:11 +0600

    Design changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ authors = User.create([
   {name: 'Sasuke', photo: 'https://cdn.myanimelist.net/images/characters/8/484628.jpg', bio: 'Sasuke Uchiha is a fictional character and one of the main characters in the Naruto manga and anime series created by Masashi Kishimoto. Sasuke is a member of the Uchiha clan, a prestigious family of ninjas known for their Sharingan, a powerful bloodline ability that allows them to copy and enhance the skills of others. Sasukes life changes dramatically after his entire family is murdered by his own older brother, Itachi. Determined to take revenge on Itachi, Sasuke leaves the Hidden Leaf Village and trains under Orochimaru, one of the legendary Sannin, to become stronger.'},
   {name: 'Hinata', photo: 'https://cdn.myanimelist.net/images/characters/15/225985.jpg?_gl=1*zedcvx*_ga*NTg1NzA0MjU1LjE2NzUzMTQ4MTE.*_ga_26FEP9527K*MTY3NTY2NTYyMi4zLjEuMTY3NTY2NTkwNC4yMy4wLjA.', bio: 'Hinata Hyuga is a fictional character in the Naruto manga and anime series created by Masashi Kishimoto. Hinata is a member of the Hyuga clan, a prestigious family of ninjas known for their Byakugan, a powerful bloodline ability that allows them to see through solid objects and see great distances. Hinata is initially portrayed as a shy and timid person, but she eventually grows into a confident and capable ninja. Throughout the series, Hinata develops a strong admiration for Naruto, the main character, and eventually falls in love with him.'},
   {name: 'Kakachi', photo: 'https://cdn.myanimelist.net/images/characters/4/307805.jpg?_gl=1*l6ptw2*_ga*NTg1NzA0MjU1LjE2NzUzMTQ4MTE.*_ga_26FEP9527K*MTY3NTY2NTYyMi4zLjEuMTY3NTY2NTY1Ny4yNS4wLjA.', bio: 'Kakashi is a skilled and experienced ninja, having participated in many battles and missions. He is also a talented teacher, having trained and mentored several generations of ninja, including Team 7. Despite his cool and reserved demeanor, Kakashi is a kind and caring person who places the safety and well-being of his friends and students above all else. He is widely regarded as one of the strongest and most capable characters in the Naruto universe, and his actions and decisions often have a significant impact on the events of the series.'},
-  {name: 'Sakura', photo: 'https://cdn.myanimelist.net/images/characters/15/103640.jpg'}
+  {name: 'Sakura', photo: 'https://cdn.myanimelist.net/images/characters/15/103640.jpg', bio: 'Sakura has a strong will and a no-nonsense attitude, and she is often the voice of reason and levelheadedness among her friends. She is also fiercely loyal to those she cares about and is willing to go to great lengths to protect them. Despite her tough exterior, Sakura also has a softer side, and she forms close bonds with her teammates and other characters throughout the series. She is widely regarded as a beloved and important character in the Naruto universe, and her development and growth are central to the story.'}
   ])
 
 posts = Post.create([
@@ -28,10 +28,10 @@ posts = Post.create([
 
 comments = Comment.create([
   {author_id: 1, post_id: 1, text: 'My Hobby Is Eating Different Kinds Of Ramen And Comparing Them.'},
-  {author_id: 4, post_id: 1, text: 'Sorry I\'m Late Guys, I Got Lost On The Path Of Life.'},
-  {author_id: 5, post_id: 1, text: 'Run Into The Setting Sun. Run And Suffer. But Don\'t Mess Up Your Hair!'},
-  {author_id: 2, post_id: 2, text: 'What Adult Goes Full Force While Fighting Mere Children?'},
-  {author_id: 3, post_id: 2, text: 'I Am Not A Pervert, I Am A Super Pervert!'}
+  {author_id: 4, post_id: 2, text: 'Sorry I\'m Late Guys, I Got Lost On The Path Of Life.'},
+  {author_id: 5, post_id: 3, text: 'Run Into The Setting Sun. Run And Suffer. But Don\'t Mess Up Your Hair!'},
+  {author_id: 2, post_id: 5, text: 'What Adult Goes Full Force While Fighting Mere Children?'},
+  {author_id: 3, post_id: 4, text: 'I Am Not A Pervert, I Am A Super Pervert!'}
 ])
 
 likes = Like.create([
