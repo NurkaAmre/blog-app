e607a74e9843fd3d8fcebcceb318b0cdf756b00d
NurkaAmre <nurkerey30@gmail.com>
2023-02-08 00:22:56 +0600

    Implemented Users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
-  root "users#index"
+  root to: "users#index"
   
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
