c36297dbab5f6cc3ff91fd9605c49fcda1c15bda
NurkaAmre <nurkerey30@gmail.com>
2023-02-07 23:20:42 +0600

    Implemented appController and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "users#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  root "users#index"
   
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
