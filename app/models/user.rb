c30f1491dda51c9467e20cc72f499688ce42c731
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-02-08 19:18:02 +0500

    Adding devise to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
-
   validates :name, presence: true
   validates :PostCounter, numericality: { greater_than_or_equal_to: 0 }
-
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
