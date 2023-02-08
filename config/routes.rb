4a5c001c440503f854759e0c0604397f13db52fa
NurkaAmre <nurkerey30@gmail.com>
2023-02-09 00:06:40 +0600

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   root to: "users#index"
   
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:create, :new]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:create, :new, :destroy]
       resources :likes, only: [:create]
     end
   end
