d40a0af898e3a7a95148d3aafd2ca4a924cc87ba
NurkaAmre <nurkerey30@gmail.com>
2023-02-10 21:39:43 +0600

    Fix errors

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,19 +5,27 @@ class Api::V1::CommentsController < Api::V1::ApplicationController
     render json: @post.comments
   end
 
+  # def create
+  #   @comment = Comment.new(comment_params)
+
+  #   if @comment.save
+  #     render json: @comment, status: :created
+  #   else
+  #     render json: @comment.errors, status: :unprocessable_entity
+  #   end
+  # end
   def create
     @comment = Comment.new(comment_params)
-
-    if @comment.save
-      render json: @comment, status: :created
-    else
-      render json: @comment.errors, status: :unprocessable_entity
-    end
+    @comment.author_id = current_user.id
+    @comment.post_id = params[:post_id]
+    post = Post.find(params[:post_id])
+    @comment.save!
+    json_response(@comment, :created)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:author_id, :post_id, :text)
+    params.require(:comment).permit(:text)
   end
 end
